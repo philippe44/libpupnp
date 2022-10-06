efff2822e7bcb86df153d687ea90e9d771f7a29b
philippe44 <philippe44@users.noreply.github.com>
2022-10-06 14:28:56 -0700

    automatic build for Windows

--- build.sh
+++ build.sh
@@ -33,21 +33,25 @@ for cc in ${compilers[@]}
 do
 	IFS=- read -r platform host dummy <<< $cc
 
+	pwd=$(pwd)
 	cd pupnp
 	export CPPFLAGS=${cppflags[$cc]} 
 	export CC=${alias[$cc]:-$cc} 
 	export CXX=${CC/gcc/g++}
 	./configure --enable-static --disable-shared --disable-samples --host=$platform-$host 
 	make clean && make
+	cd $pwd
 		
-	mkdir -p ../targets/$host/$platform
-	cp upnp/.libs/lib*.a $_
-	cp ixml/.libs/lib*.a $_
-	mkdir -p ../targets/$host/$platform/include
-	cp -ur upnp/inc/* $_
-	cp -ur ixml/inc/* $_
-	find $_ -type f -not -name "*.h" -exec rm {} +
-	cd ..
+	target=targets/$host/$platform
+	mkdir -p $target
+	mkdir -p $_/include
+	for item in upnp ixml
+	do
+		cp pupnp/$item/.libs/lib$item.a $target
+		ar -rc --thin $_/libpupnp.a $_/lib$item.a 
+		cp -ur pupnp/$item/inc/* $target/include
+		find $_ -type f -not -name "*.h" -exec rm {} +
+	done	
 done
 
 
